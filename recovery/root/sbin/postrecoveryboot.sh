942d0802810f48b1fbdb97407dd3141afe6ac393
NFS-Support <jarbull87@gmail.com>
2021-08-21 19:41:44 +0700

    Update postrecoveryboot.sh

--- recovery/root/sbin/postrecoveryboot.sh
+++ recovery/root/sbin/postrecoveryboot.sh
@@ -20,14 +20,7 @@
 #
 echo battery-charging > /sys/class/leds/blue/trigger
 echo battery-full > /sys/class/leds/green/trigger
-echo bkl-trigger > /sys/class/leds/button-backlight/trigger
-
-# Enable keys by default
-echo 5 > /sys/class/leds/button-backlight/brightness
 
 echo "0" > /sys/class/leds/led:torch_1/max_brightness
-echo "0" > /sys/class/leds/torch-light1/max_brightness
+echo "0" > /sys/class/leds/torch-light0/max_brightness
 echo "0" > /sys/class/leds/led:flash_1/max_brightness
-
-echo "0" > /proc/touchpanel/capacitive_keys_disable
-# echo "0" > /sys/devices/soc/qpnp-flash-led-25/leds/led:torch_1/max_brightness
